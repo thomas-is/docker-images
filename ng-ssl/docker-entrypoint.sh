c0724952e27e332c57a6e44d79a78a5554a9d275
thomas-is <thomas@0l.fi>
2020-07-06 13:54:54 +0000

    added ng-ssl

--- ng-ssl/docker-entrypoint.sh
+++ ng-ssl/docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-certbot certonly --standalone --non-interactive --agree-tos \
-  -m $CERTBOT_MAIL \
-  -d $CERTBOT_DOMAIN
-
-sed -i -e "s/\${CERTBOT_DOMAIN}/$CERTBOT_DOMAIN/g" /etc/nginx/conf.d/default.conf
+# nginx conf
+sed -i -e "s/\${SSL_DOMAIN}/$SSL_DOMAIN/g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
