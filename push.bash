dc9bc0ce68390503eac36571c5175efc1977458d
thomas-is <thomas@0l.fi>
2020-07-11 10:31:46 +0200

    ignore dirs with no Dockerfile

--- push.bash
+++ push.bash
@@ -5,6 +5,7 @@ docker login -u 0lfi
 
 for TAG in $( ls -d */ | sed -e 's/\///g' )
 do
+  if [ ! -f $TAG/Dockerfile ] ; then continue ; fi
   echo "docker build -t 0lfi/$TAG ./$TAG"
   docker build -t 0lfi/$TAG ./$TAG
   echo "docker push 0lfi/$TAG"
