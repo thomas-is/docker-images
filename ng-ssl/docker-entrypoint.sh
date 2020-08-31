9f2b74ab3ab9facf8be4c881c50d5c32135e1335
thomas-is <thomas@0l.fi>
2020-08-31 08:17:03 +0200

    certbot: added -d ${SSL_DOMAIN}

--- ng-ssl/docker-entrypoint.sh
+++ ng-ssl/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# nginx conf
 sed -i -e "s/\${SSL_DOMAIN}/$SSL_DOMAIN/g" /etc/nginx/conf.d/default.conf
+sed -i -e "s/\${SSL_DOMAIN}/$SSL_DOMAIN/g" /etc/crontabs/root
 
 crond -f -l 8 &
 
