090a031fd2ee4f7e203209f00e615998bc8a8092
thomas-is <thomas@0l.fi>
2020-09-19 10:10:34 +0200

    added --rm switch

--- scripts/vcopy.bash
+++ scripts/vcopy.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # copy volume $1 to volue $2
-docker run -it -v $1:/src -v $2:/dst alpine cp -al /src /dst
+docker run --rm -it \
+  -v $1:/src \
+  -v $2:/dst \
+  alpine \
+  cp -al /src /dst
