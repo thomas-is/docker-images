7553815b8f43798218522ef699fb5a5420505a5f
thomas-is <thomas@0l.fi>
2020-09-25 07:02:02 +0200

    nfs+stunnel client and server

---
+++ nfs-client/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -f /run/secrets/${NFS_PSK} ] ; then
+  echo "Can't find docker secret ${NFS_PSK}"
+  echo "Can't run without it!"
+  exit 1
+fi
+
+cat > /etc/stunnel/stunnel.conf <<EOF
+foreground = yes
+setuid = root
+setgid = root
+pid = /run/stunnel.pid
+output = /dev/stdout
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+client = yes
+
+[nfs]
+accept  = 0.0.0.0:2049
+connect = ${NFS_SERVER}:2049
+ciphers = PSK
+PSKsecrets = /run/secrets/${PSK}
+EOF
+
+exec "$@"
---
+++ nfs-server/docker-entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ ! -f /run/secrets/${NFS_PSK} ] ; then
+  echo "Can't find docker secret ${NFS_PSK}"
+  echo "Can't run without it!"
+  exit 1
+fi
+
+cat > /etc/exports <<EOF
+/nfs    127.0.0.1(${NFS_OPTIONS})
+EOF
+
+cat > /etc/stunnel/stunnel.conf <<EOF
+foreground = yes
+setuid = root
+setgid = root
+pid = /run/stunnel.pid
+output = /dev/stdout
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+client = no
+
+[nfs]
+accept  = 0.0.0.0:2049
+connect = 127.0.0.1:2049
+ciphers = PSK
+PSKsecrets = /run/secrets/${PSK}
+EOF
+
+exportfs -arv
+
+
+exec "$@"
