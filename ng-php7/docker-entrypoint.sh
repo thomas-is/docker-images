07106a07c880be833ea1d713dce77f1e509d23fb
thomas-is <thomas@0l.fi>
2021-01-16 11:19:33 +0100

    :wrench: added PHP_UID, PHP_GID env vars

--- ng-php7/docker-entrypoint.sh
+++ ng-php7/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -e "s/user = nobody/user = $PHP_UID/g" -i /etc/php7/php-fpm.d/www.conf
+sed -e "s/group = nobody/group = $PHP_GID/g" -i /etc/php7/php-fpm.d/www.conf
+
 if [ "$PHP_CLEAR_ENV" = "no" ] ; then
   sed -e 's/;clear_env/clear_env/g' -i /etc/php7/php-fpm.d/www.conf
 fi
