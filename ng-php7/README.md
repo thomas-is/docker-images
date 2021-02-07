56669c37051e7d5474fd1c3061f07c869dd83c43
thomas-is <thomas@0l.fi>
2021-02-07 12:12:48 +0100

    :wrench: PHP_USER
    - use PHP_USER instead of PHP_UID, PHP_GID
    - can be root
    - updated README

--- ng-php7/README.md
+++ ng-php7/README.md
@@ -6,3 +6,12 @@
 docker run -d --rm -p 8080:80 0lfi/ng-php7
 ```
 <http://localhost:8080> will display `phpinfo()`
+
+## Environment variables
+
+- `PHP_USER`
+  - default `nobody`
+  - can be set as `root` (**use at your own risk**)
+- `PHP_CLEAR_ENV`
+  - default `yes`
+  - set explicitly to `no` to access environment variables from PHP
--- ng-php7/docker-entrypoint.sh
+++ ng-php7/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sed -e "s/user = nobody/user = $PHP_UID/g" -i /etc/php7/php-fpm.d/www.conf
-sed -e "s/group = nobody/group = $PHP_GID/g" -i /etc/php7/php-fpm.d/www.conf
+sed -e "s/user = nobody/user = $PHP_USER/g" -i /etc/php7/php-fpm.d/www.conf
+sed -e "s/group = nobody/group = $PHP_USER/g" -i /etc/php7/php-fpm.d/www.conf
 
 if [ "$PHP_CLEAR_ENV" = "no" ] ; then
   sed -e 's/;clear_env/clear_env/g' -i /etc/php7/php-fpm.d/www.conf
 fi
 
-php-fpm7
+php-fpm7 --allow-to-run-as-root
 
 exec "$@"
