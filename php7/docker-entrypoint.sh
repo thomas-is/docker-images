6d99b0c02ca23b9210051b28009f9173499b3791
thomas-is <thomas@0l.fi>
2021-05-03 10:34:39 +0200

    :construction: add standalone php7 fpm

---
+++ php7/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$(id -u)" = 0 ] ; then
+
+  sed -e "s/user = nobody/user = $FPM_USER/g" -i /etc/php7/php-fpm.d/www.conf
+  sed -e "s/group = nobody/group = $FPM_USER/g" -i /etc/php7/php-fpm.d/www.conf
+
+  if [ "$FPM_CLEAR_ENV" = "no" ] ; then
+    sed -e 's/;clear_env/clear_env/g' -i /etc/php7/php-fpm.d/www.conf
+  fi
+
+fi
+
+exec "$@"
