7709a379b11fa5fdb1098000427f96ef48c4de98
thomas-is <thomas@0l.fi>
2021-05-23 11:47:45 +0200

    :whale: run tor as USER_ID, enable tor control port with random password

--- firefox/docker-entrypoint.sh
+++ firefox/docker-entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-tor &
-
 usermod firefox -u $USER_ID
+TOR_PASS=$( date +%s | sha256sum | base64 | head -c 32 ; echo )
+HASH=$( su firefox -c "tor --hash-password \"$TOR_PASS\"" )
+
+cat <<-EOF > /etc/tor/torrc
+  Log notice file /var/log/tor/notices.log
+  DataDirectory /var/lib/tor
+  ControlPort 9051
+  HashedControlPassword ${HASH}
+EOF
+
+echo "$HASH" > /home/firefox/torpass
+chmod 644 /home/firefox/torpass
+
+su firefox -c "tor" &
+
 CMD="$@"
-/bin/su firefox -p -c "$CMD"
+su firefox -p -c "$CMD"
