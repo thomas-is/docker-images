ccbb5bb423894ebe0e51a56447a7ed052a0c958d
thomas-is <thomas@0l.fi>
2021-05-23 12:10:03 +0200

    :shell: add newnym command

--- firefox/docker-entrypoint.sh
+++ firefox/docker-entrypoint.sh
@@ -2,17 +2,17 @@
 
 usermod firefox -u $USER_ID
 TOR_PASS=$( date +%s | sha256sum | base64 | head -c 32 ; echo )
-HASH=$( su firefox -c "tor --hash-password \"$TOR_PASS\"" )
+echo "$TOR_PASS" > /home/firefox/torpass
+chmod 644 /home/firefox/torpass
 
+HASH=$( su firefox -c "tor --hash-password \"$TOR_PASS\"" )
 cat <<-EOF > /etc/tor/torrc
   Log notice file /var/log/tor/notices.log
   DataDirectory /var/lib/tor
   ControlPort 9051
   HashedControlPassword ${HASH}
+  CookieAuthentication 0
 EOF
-
-echo "$HASH" > /home/firefox/torpass
-chmod 644 /home/firefox/torpass
 
 su firefox -c "tor" &
 
