29d9cd4fefc846021ee819aa30d542bc09dce47e
thomas-is <thomas@0l.fi>
2022-03-02 07:51:48 +0100

    :package: use autoconfig.js

--- firefox/docker-entrypoint.sh
+++ firefox/docker-entrypoint.sh
@@ -14,7 +14,17 @@ cat <<-EOF > /etc/tor/torrc
   CookieAuthentication 0
 EOF
 
+su firefox -c 'firefox -CreateProfile "main /home/firefox/profile"'
+
+su firefox -c 'touch /home/firefox/profile/pref.js'
+
+cat <<-EOF > /home/firefox/profile/pref.js
+user_pref("network.proxy.socks", "127.0.0.1");
+user_pref("network.proxy.socks_port", 9050);
+user_pref("network.proxy.type", 1);
+EOF
+
 su firefox -c "tor" &
 
 CMD="$@"
-su firefox -p -c "$CMD"
+su firefox -p main -c "$CMD"
