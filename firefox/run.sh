55c3d497fb548fca08c82d5866b9aceb2c9cfb6d
thomas-is <thomas@0l.fi>
2022-03-05 19:47:27 +0100

    :whale: add pciutils, mesa-dri

--- firefox/run.sh
+++ firefox/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 docker run --rm -it \
-    --name firefox \
-    --shm-size=2048m \
-    -e USER_ID=$(id -u) \
-    -e HOME=/home/firefox \
-    -e DISPLAY=unix$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $HOME/.Xauthority:/firefox/.Xauthority:ro \
-    -e PULSE_SERVER=unix:/pulse \
-    -v /run/user/$(id -u)/pulse/native:/pulse \
-    0lfi/firefox "$@"
+  --name firefox \
+  --shm-size=2048m \
+  --device /dev/dri \
+  --device /dev/vga_arbiter \
+  -e USER_ID=$(id -u) \
+  -e HOME=/home/firefox \
+  -e DISPLAY=unix$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $HOME/.Xauthority:/firefox/.Xauthority:ro \
+  -e PULSE_SERVER=unix:/pulse \
+  -v /run/user/$(id -u)/pulse/native:/pulse \
+  0lfi/firefox "$@"
--- firefox/test.sh
+++ firefox/test.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-docker build -t test .
+docker build -t firefox .
 
 docker run --rm -it \
-    --name firefox \
-    --shm-size=2048m \
-    -e USER_ID=$(id -u) \
-    -e HOME=/home/firefox \
-    -e DISPLAY=unix$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $HOME/.Xauthority:/firefox/.Xauthority:ro \
-    -e PULSE_SERVER=unix:/pulse \
-    -v /run/user/$(id -u)/pulse/native:/pulse \
-    test "$@"
-#    -v $(pwd)/mozilla:/home/firefox/.mozilla \
+  --name firefox \
+  --shm-size=2048m \
+  --device /dev/dri \
+  --device /dev/vga_arbiter \
+  -e USER_ID=$(id -u) \
+  -e HOME=/home/firefox \
+  -e DISPLAY=unix$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $HOME/.Xauthority:/firefox/.Xauthority:ro \
+  -e PULSE_SERVER=unix:/pulse \
+  -v /run/user/$(id -u)/pulse/native:/pulse \
+  firefox "$@"
