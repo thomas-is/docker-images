5365a55fcdfcea2d5ad01619e1c0a7b82099facc
thomas-is <thomas@0l.fi>
2022-03-13 12:13:38 +0100

    :whale: add PROFILE env, ttf-dejavu

--- firefox/docker-entrypoint.sh
+++ firefox/docker-entrypoint.sh
@@ -4,6 +4,7 @@ usermod firefox -u $USER_ID
 TOR_PASS=$( date +%s | sha256sum | base64 | head -c 32 ; echo )
 echo "$TOR_PASS" > /home/firefox/torpass
 chmod 644 /home/firefox/torpass
+chown firefox:firefox /home/firefox/torpass
 
 HASH=$( su firefox -c "tor --hash-password \"$TOR_PASS\"" )
 cat <<-EOF > /etc/tor/torrc
@@ -15,6 +16,11 @@ cat <<-EOF > /etc/tor/torrc
 EOF
 
 su firefox -c "tor" &
+
+su firefox -c "mkdir -p /home/firefox/downloads"
+su firefox -c "mkdir -p /home/firefox/.cache"
+su firefox -c "mkdir -p /home/firefox/.mozilla/firefox/$PROFILE"
+su firefox -c "firefox -CreateProfile \"$PROFILE /home/firefox/.mozilla/firefox/$PROFILE\""
 
 CMD="$@"
 su firefox -c "$CMD"
