0c6ec184586fc1c155ca71431d3bd26d0bd40758
thomas-is <thomas@0l.fi>
2022-03-30 23:38:43 +0200

    :package: add pulseaudio plugin and winetricks dependancies

--- wine/docker-entrypoint.sh
+++ wine/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 usermod wine -u $USER_ID
+su wine -c "/usr/local/bin/winetricks d3dx9"
+su wine -c "/usr/local/bin/winetricks d3dx9_41"
 CMD="$@"
-su wine -c "$CMD"
+#su wine -c "$CMD"
+exec $CMD
