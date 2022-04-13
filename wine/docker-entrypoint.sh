8f0f9a74c29bc6f28095f6c2cbcca85bd990e27c
thomas-is <thomas@0l.fi>
2022-04-13 23:21:43 +0200

    :construction: dxvk
    - switch to alpine:edge
    - enable testing repo
    - add dxvk
    - fix render and video groups
    - use d3d9_41 for test (no dxvk)
    - add sudo
    - enable wine to sudo without password

--- wine/docker-entrypoint.sh
+++ wine/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#addgroup -g $VIDEO_GID video
+groupmod -g $VIDEO_GID video
+addgroup -g $RENDER_GID render
+
+usermod -a -G video  wine
+usermod -a -G render wine
 usermod wine -u $USER_ID
 
 # skip MONO install on init
@@ -12,3 +18,4 @@ fi
 
 CMD="$@"
 su wine -c "$CMD"
+#exec "$@"
--- wine/test.sh
+++ wine/test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-docker build -t wine --build-arg ALPINE=edge . || exit 1
+docker build -t wine . || exit 1
+
+xhost +
 
 docker run --rm -it \
   --name wine \
@@ -8,6 +10,8 @@ docker run --rm -it \
   --device /dev/dri \
   --device /dev/vga_arbiter \
   -e USER_ID=$(id -u) \
+  -e RENDER_GID=$( cat /etc/group | grep render | cut -f3 -d":" ) \
+  -e VIDEO_GID=$( cat /etc/group | grep video  | cut -f3 -d":" ) \
   -e WINETRICKS="isolate_home d3dx9_41" \
   -e HOME=/home/wine \
   -e DISPLAY=unix$DISPLAY \
@@ -17,3 +21,5 @@ docker run --rm -it \
   -v /run/user/$(id -u)/pulse/native:/pulse \
   -v ~/games/wine:/home/wine/games \
   wine "$@"
+#  --group-add render \
+#  --group-add video \
