f2f1ef83aabc5298cb381da33e79419fe50c30d6
thomas-is <thomas@0l.fi>
2022-05-02 17:42:40 +0200

    :whale: remove FPM_CLEAR_ENV, disable php expose version

--- ng-php8/docker-entrypoint.sh
+++ ng-php8/docker-entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-if [ "$(id -u)" = 0 ] ; then
+# remove the X-Powered-By header
+sed -e "s/expose_php = On/expose_php = Off/g" -i /etc/php8/php.ini
 
-  sed -e "s/user = nobody/user = $FPM_USER/g"   -i /etc/php8/php-fpm.d/www.conf
-  sed -e "s/group = nobody/group = $FPM_USER/g" -i /etc/php8/php-fpm.d/www.conf
+sed -e "s/user = nobody/user = $FPM_USER/g"   -i /etc/php8/php-fpm.d/www.conf
+sed -e "s/group = nobody/group = $FPM_USER/g" -i /etc/php8/php-fpm.d/www.conf
+sed -e 's/;clear_env/clear_env/g'             -i /etc/php8/php-fpm.d/www.conf
 
-  if [ "$FPM_CLEAR_ENV" = "no" ] ; then
-    sed -e 's/;clear_env/clear_env/g' -i /etc/php8/php-fpm.d/www.conf
-  fi
-
-  if [ "$LOG_FORMAT" != "main" ] ; then
-    sed -e "s/access_log \/dev\/stdout main;/access_log \/dev\/stdout $LOG_FORMAT;/g" -i /etc/nginx/http.d/default.conf
-  fi
-
-  php-fpm8 --allow-to-run-as-root
-
+if [ "$LOG_FORMAT" != "main" ] ; then
+  sed -e "s/access_log \/dev\/stdout main;/access_log \/dev\/stdout $LOG_FORMAT;/g" -i /etc/nginx/http.d/default.conf
 fi
+
+php-fpm8 --allow-to-run-as-root
 
 exec "$@"
