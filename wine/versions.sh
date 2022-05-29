d59d6b24304aca858f833d09217e64c2fe1105db
thomas-is <thomas@0l.fi>
2022-05-29 12:21:08 +0200

    :recycle: remove wine
    see <https://github.com/thomas-is/wine-docker> instead

--- wine/versions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for ALPINE in latest edge
-do
-  docker pull alpine:$ALPINE > /dev/null
-  WINE=$( docker run --rm -it alpine:$ALPINE sh -c "apk update > /dev/null && apk search wine" | grep -v "^wine-d" | grep "^wine-" | cut -f2 -d"-" )
-  printf "alpine-%s:wine-%s\n" $ALPINE $WINE
-done
