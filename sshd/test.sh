0e631e9de1115647cdfb1dba1dc2f9cad01170c2
thomas-is <thomas@0l.fi>
2022-12-14 09:55:19 +0100

    :shell: improve test script

--- sshd/test.sh
+++ sshd/test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+BASE=$( realpath $( dirname $0 ) )
+
+mkdir -p $BASE/ssh
+
 docker build -t sshd . || exit 1
 docker run --rm -it \
   --name sshd \
   -e USER=dev \
-  -e SHELL=/bin/false \
-  -v /home/debian/.ssh/authorized_keys:/user/.ssh/authorized_keys:ro \
+  -e SHELL=/bin/sh \
+  -v $HOME/.ssh/authorized_keys:/user/.ssh/authorized_keys:ro \
+  -v $BASE/ssh:/etc/ssh \
   -p 2222:22 \
   sshd
