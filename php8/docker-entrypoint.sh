6b72de0f16d970ddd643019ce5c2d8c9a34cc158
thomas-is <thomas@0l.fi>
2024-06-12 18:53:18 +0200

    🆙 use alpine 3.20 and php 8.3

--- php8/docker-entrypoint.sh
+++ php8/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# non root user won't be able to edit fpm conf
 if [ "$(id -u)" = 0 ] ; then
-  sed -e "s/user = nobody/user = $FPM_USER/g" -i /etc/php8/php-fpm.d/www.conf
-  sed -e "s/group = nobody/group = $FPM_USER/g" -i /etc/php8/php-fpm.d/www.conf
-  sed -e 's/;clear_env/clear_env/g' -i /etc/php8/php-fpm.d/www.conf
+  sed -e "s/user = nobody/user = $FPM_USER/g"   -i /etc/php83/php-fpm.d/www.conf
+  sed -e "s/group = nobody/group = $FPM_USER/g" -i /etc/php83/php-fpm.d/www.conf
+  sed -e 's/;clear_env/clear_env/g'             -i /etc/php83/php-fpm.d/www.conf
 fi
 
 exec "$@"
