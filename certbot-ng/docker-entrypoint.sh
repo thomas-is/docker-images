1943d3a60ef246ea150138e58fe3e479279d53d0
thomas-is <thomas@0l.fi>
2024-06-13 18:44:20 +0200

    🐳 add RENEW_SCHEDULE, some clean up

--- certbot-ng/docker-entrypoint.sh
+++ certbot-ng/docker-entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-printf "# m	h	d	M	w	command\n  0	4	*	*	*	certbot renew --nginx -n\n\n" > /etc/crontabs/root
+if [ $(id -u) -ne 0 ]; then
+  echo "[entrypoint] can't run as non-root user!"
+  exit 1
+fi
 
+echo "[entrypoint] crontab generation"
+cat << EOF > /etc/crontabs/root
+# m h d M w command
+  $RENEW_SCHEDULE certbot renew --nginx -n
+#
+EOF
+cat /etc/crontabs/root
+
+echo "[entrypoint] starting crond"
 crond -f -l 8 &
 
+echo "[entrypoint] end of entrypoint"
 exec "$@"
