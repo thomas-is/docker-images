1700ff0b524b9f55116990d7c75083de4ddf4cea
thomas-is <thomas@0l.fi>
2024-09-21 11:50:33 +0200

    🚧 add firefox (debian based)

---
+++ firefox-debian/docker-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+CMD="$@"
+su firefox -c "$CMD"
---
+++ firefox-debian/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+docker build -t firefox . || exit 1
+
+docker run --rm -it \
+  --name firefox \
+  --shm-size=2048m \
+  --device /dev/dri \
+  --device /dev/vga_arbiter \
+  -e USER_ID=$(id -u) \
+  -e HOME=/home/firefox \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v $HOME/.Xauthority:/firefox/.Xauthority:ro \
+  -e PULSE_SERVER=unix:/pulse \
+  -v /run/user/$(id -u)/pulse/native:/pulse \
+  firefox "$@"
