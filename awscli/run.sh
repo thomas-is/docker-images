d25cd7f80a539bbdd56c298c31a1d067855abd84
thomas-is <thomas@0l.fi>
2024-11-05 12:38:46 +0100

    🐚 add bind mounts

--- awscli/run.sh
+++ awscli/run.sh
@@ -3,7 +3,12 @@
 base=$( realpath $(dirname $0) )
 
 docker build -t awscli $base || exit 1
+
+mkdir -p ~/.aws
+mkdir -p ~/lab/aws/src
+
 docker run --rm -it \
   --name awscli \
   -v ~/.aws:/home/aws/.aws \
+  -v ~/lab/aws/src:/home/aws/src \
   awscli
