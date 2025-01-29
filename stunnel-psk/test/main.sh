eb1215852365c46e6fdce476bc47b0703dae1a58
thomas-is <thomas@0l.fi>
2025-01-29 10:52:05 +0100

    📦 add test with stack example

---
+++ stunnel-psk/test/main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+base=$( realpath $( dirname $0 ) )
+
+cd $base/..
+
+echo "🐳 docker build"
+docker build -t stunnel-test . || exit 1
+
+cd $base
+
+echo "⚙️  PSK generation"
+echo "testing:$( echo $(openssl rand 64 | base64 ))" > ./psk
+
+echo "🐳 docker stack deploy"
+docker stack deploy -c tunnel.yml tunnel
+
+echo "😪 waiting a few seconds"
+sleep 5
+
+echo "⚙️  curl"
+curl http://127.0.0.1:8080
